f7d88f9b4676f544973bc2c11039a1662d77b285
RenattoGuzman <renttogs@gmail.com>
2023-11-29 12:57:18 -0600

    api_inicial

---
+++ api/index.js
@@ -0,0 +1,8 @@
+const app = require('express')(); 
+const port = 8080;
+
+app.listen(
+    port, 
+    () => console.log(`Funcionaaaaa en el localhost:${port}`)
+    
+)
---
+++ api/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const csv = require('csv-parser');
+const fs = require('fs');
+
+const app = express();
+const PORT = 3001;
+
+app.get('/data', (req, res) => {
+  const results = [];
+
+  fs.createReadStream('spotify_songs.csv')
+    .pipe(csv())
+    .on('data', (data) => results.push(data))
+    .on('end', () => res.json(results));
+});
+
+app.get('/most_popular_songs', (req, res) => {
+  const results = [];
+
+  fs.createReadStream('spotify_songs.csv')
+    .pipe(csv())
+    .on('data', (data) => results.push(data))
+    .on('end', () => {
+      results.sort((a, b) => b.track_popularity - a.track_popularity);
+      
+      const uniqueResults = [];
+      const uniqueTrackNames = new Set();
+
+      for (const result of results) {
+        if (!uniqueTrackNames.has(result.track_name)) {
+          uniqueResults.push(result);
+          uniqueTrackNames.add(result.track_name);
+        }
+      }
+      const top10Results = uniqueResults.slice(0, 10);
+
+      res.json(top10Results);
+
+    });
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
